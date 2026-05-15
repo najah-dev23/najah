fc1c7eb6d8f4faca2dd93e7abe3c3426fa53963d
najah.academy (via WeWeb) <najah.academy.ar@gmail.com>
2026-05-16 00:49:04 +0200

    v235 - PREP 1.7 (SKELETON QUIZZ TEST)

--- src/pages/4d9adb8d-3310-46fe-a877-8b466df1b77f.js
+++ src/pages/4d9adb8d-3310-46fe-a877-8b466df1b77f.js
@@ -10,6 +10,7 @@ import element_aa29a661_07ce_484e_8abb_456186211282 from '@/components/elements/
 import element_b783dc65_d528_4f74_8c14_e27c934c39b1 from '@/components/elements/element-b783dc65-d528-4f74-8c14-e27c934c39b1/src/wwElement.vue';
 import element_c6c0c00e_49fd_4cb9_bd78_5bc09945721e from '@/components/elements/element-c6c0c00e-49fd-4cb9-bd78-5bc09945721e/src/wwElement.vue';
 import element_d7904e9d_fc9a_4d80_9e32_728e097879ad from '@/components/elements/element-d7904e9d-fc9a-4d80-9e32-728e097879ad/src/wwElement.vue';
+import element_f4c24d06_bc88_4b62_add3_4d1f8b42221c from '@/components/elements/element-f4c24d06-bc88-4b62-add3-4d1f8b42221c/src/wwElement.vue';
 
 // eslint-disable-next-line no-undef
 app.component('wwobject-1b1e2173-9b78-42cc-a8ee-a6167caea340', element_1b1e2173_9b78_42cc_a8ee_a6167caea340);
@@ -20,6 +21,7 @@ app.component('wwobject-aa29a661-07ce-484e-8abb-456186211282', element_aa29a661_
 app.component('wwobject-b783dc65-d528-4f74-8c14-e27c934c39b1', element_b783dc65_d528_4f74_8c14_e27c934c39b1);
 app.component('wwobject-c6c0c00e-49fd-4cb9-bd78-5bc09945721e', element_c6c0c00e_49fd_4cb9_bd78_5bc09945721e);
 app.component('wwobject-d7904e9d-fc9a-4d80-9e32-728e097879ad', element_d7904e9d_fc9a_4d80_9e32_728e097879ad);
+app.component('wwobject-f4c24d06-bc88-4b62-add3-4d1f8b42221c', element_f4c24d06_bc88_4b62_add3_4d1f8b42221c);
 
 // eslint-disable-next-line no-undef
 import section_99586bd3_2b15_4d6b_a025_6a50d07ca845 from '@/components/sections/section-99586bd3-2b15-4d6b-a025-6a50d07ca845/src/wwSection.vue';
