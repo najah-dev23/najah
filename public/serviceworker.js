ef8d3bfb7e242e91a40a3b6dbebbe7e75097c2b2
najah.academy (via WeWeb) <najah.academy.ar@gmail.com>
2026-07-12 17:47:03 +0000

    v305 - PROD 8.2

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,4 +1,4 @@
-const version = 304;
+const version = 305;
 self.addEventListener('install', event => {
     // eslint-disable-next-line no-console
     console.log(`Service worker v${version} installed`);
